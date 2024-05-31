8af7df74f8ffb42007ee0e69b3b70c2cdcd6a4de
DaeYang <wkdeodid02@gmail.com>
2024-05-31 10:55:08 +0900

    new

--- membership/membership.js
+++ membership/membership.js
@@ -4,3 +4,7 @@ const menuButton = document.querySelector(".navbar__icons")
 menuButton.addEventListener("click", () => {
     menu.classList.toggle("navbar__open");
 });
+document.getElementById('support-button').addEventListener('click', function() {
+    var supportPanel = document.getElementById('support-panel');
+    supportPanel.style.display = supportPanel.style.display === 'none' ? 'block' : 'none';
+});
--- 서비스 소개/maketing.js
+++
@@ -1,6 +0,0 @@
-//manu
-const menu = document.querySelector(".navbar__links")
-const menuButton = document.querySelector(".navbar__icons")
-menuButton.addEventListener("click", () => {
-    menu.classList.toggle("navbar__open");
-});
