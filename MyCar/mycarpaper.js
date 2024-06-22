4a002c9d8840a8f17e22a0f6ee751336357b81bb
DaeYang <wkdeodid02@gmail.com>
2024-06-22 11:45:48 +0900

    new

--- MyCar/mycarpaper.js
+++ MyCar/mycarpaper.js
@@ -8,27 +8,3 @@ document.getElementById('support-button').addEventListener('click', function() {
     var supportPanel = document.getElementById('support-panel');
     supportPanel.style.display = supportPanel.style.display === 'none' ? 'block' : 'none';
 });
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const icon = document.getElementById('icon');
-//     const dropdownMenu = document.getElementById('dropdown-menu');
-
-//     function showMenu() {
-//         dropdownMenu.classList.add('show');
-//     }
-
-//     function hideMenu() {
-//         setTimeout(function() {
-//             if (!icon.matches(':hover') && !dropdownMenu.matches(':hover')) {
-//                 dropdownMenu.classList.remove('show');
-//             }
-//         }, 200);
-//     }
-
-//     icon.addEventListener('mouseenter', showMenu);
-//     icon.addEventListener('mouseleave', hideMenu);
-//     dropdownMenu.addEventListener('mouseenter', showMenu);
-//     dropdownMenu.addEventListener('mouseleave', hideMenu);
-// });
